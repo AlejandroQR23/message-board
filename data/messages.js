743a87d6a3b8b786b713ff277fab743912803579
AlejandroQR23 <alejandro.quijano@comunidad.unam.mx>
2021-08-29 16:17:12 -0500

    messages now are on another file to use them in both new and index routes

---
+++ data/messages.js
@@ -0,0 +1,13 @@
+const messages = [
+  { text: 'Hello there!', user: 'Alejandro', added: new Date().toDateString() },
+  { text: 'Hello there x2!', user: 'Amanda', added: new Date().toDateString() },
+  {
+    text: 'Hello there x3!',
+    user: 'Roberto',
+    added: new Date().toDateString(),
+  },
+];
+
+module.exports = {
+  messages,
+};
